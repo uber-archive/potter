507f66430a3f338231ae62fc1f53e9a6dc69ae48
Andrew de Andrade <aandrade@uber.com>
2014-11-07 12:37:12 -0800

    Refactored version tests

--- test/version.js
+++ test/version.js
@@ -1,30 +1,26 @@
 'use strict';
 var test = require('tape');
-var parallel = require('run-parallel');
-
+var fmt = require('util').format;
 var potter = require('./lib/potter.js');
 var expectedVersion = require('../package.json').version;
 
-test('potter version', function t(assert) {
-    potter('version', function onVersion(err, stdout, stderr) {
-        assert.ifError(err);
-        assert.equal(stderr, '');
-        assert.equal(stdout, expectedVersion + '\n');
-        assert.end();
-    });
-});
+function getVersionWith(cmd, callback) {
+    var label = fmt('print potter version with `potter %s`', cmd);
+    test(label, t);
 
-test('potter version aliases', function t(assert) {
-    parallel({
-        v: potter.bind(null, '-v'),
-        version: potter.bind(null, '--version')
-    }, function (err, results) {
-        assert.ifError(err);
-        var keys = Object.keys(results);
-        keys.forEach(function (key) {
-            var stdout = results[key];
-            assert.equal(stdout, expectedVersion + '\n');
-        });
-        assert.end();
-    });
-});
+    function t(assert) {
+        potter(cmd, assertVersionOutput);
+
+        function assertVersionOutput(err, stdout, stderr) {
+            assert.ifError(err, 'No error');
+            assert.equal(stderr, '', 'No output on stderr');
+            assert.equal(stdout, expectedVersion + '\n',
+                'Correct version on stdout');
+            assert.end();
+        }
+    }
+}
+
+getVersionWith('version');
+getVersionWith('--version');
+getVersionWith('-v');
