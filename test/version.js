4def4ca263709b68e2cc16c7decf78fa64660e75
Andrew de Andrade <aandrade@uber.com>
2014-11-07 12:41:40 -0800

    Refactored version tests using partials

--- test/version.js
+++ test/version.js
@@ -1,6 +1,8 @@
 'use strict';
 var test = require('tape');
 var fmt = require('util').format;
+var partial = require('partial');
+
 var potter = require('./lib/potter.js');
 var expectedVersion = require('../package.json').version;
 
@@ -9,16 +11,15 @@ function getVersionWith(cmd, callback) {
     test(label, t);
 
     function t(assert) {
-        potter(cmd, assertVersionOutput);
-
-        function assertVersionOutput(err, stdout, stderr) {
-            assert.ifError(err, 'No error');
-            assert.equal(stderr, '', 'No output on stderr');
-            assert.equal(stdout, expectedVersion + '\n',
-                'Correct version on stdout');
-            assert.end();
-        }
+        potter(cmd, partial(assertVersionOutput, assert));
     }
+}
+
+function assertVersionOutput(assert, err, stdout, stderr) {
+    assert.ifError(err, 'No error');
+    assert.equal(stderr, '', 'No output on stderr');
+    assert.equal(stdout, expectedVersion + '\n', 'Correct version on stdout');
+    assert.end();
 }
 
 getVersionWith('version');
